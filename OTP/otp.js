abaf0a7c4fde951392d65e043aaa002e2bae0f6f
Subhradip Ghosh <subhra2628@gmail.com>
2026-03-30 20:48:37 +0530

    update otp.js

--- OTP/otp.js
+++ OTP/otp.js
@@ -1,3 +1,54 @@
+// const express = require("express");
+// const router = express.Router();
+// const db = require("../Config/Db");
+// const nodemailer = require("nodemailer");
+
+// // OTP generator
+// function generateOTP() {
+//   return Math.floor(100000 + Math.random() * 900000).toString();
+// }
+
+// // transporter
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER, // email
+//     pass: process.env.EMAIL_PASS, // app password
+//   },
+// });
+
+// router.post("/", async (req, res) => {
+//   try {
+//     const { email } = req.body;
+//     if (!email) return res.status(400).json({ message: "Email required" });
+// // Delete old OTPs for this email
+// await db.query("DELETE FROM otp_codes WHERE email=?", [email]);
+//     const emailOtp = generateOTP();
+//     const expiresAt = new Date(Date.now() + 5 * 60 * 1000); // 5 min
+
+//     // Save OTP in DB
+//     await db.query(
+//       "INSERT INTO otp_codes (email, email_otp, expires_at) VALUES (?, ?, ?)",
+//       [email, emailOtp, expiresAt]
+//     );
+
+//     // Send OTP via email
+//     await transporter.sendMail({
+//       from: `"abhinandancaterer",<${process.env.EMAIL_USER}>`,
+//       to: email,
+//       subject: "Your OTP Code",
+//       text: `Your OTP is: ${emailOtp}. It will expire in 5 minutes.`,
+//     });
+
+//     console.log(`OTP for ${email}: ${emailOtp}`);
+//     res.json({ message: "OTP sent successfully" });
+//   } catch (err) {
+//     console.error("OTP ERROR:", err);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// });
+
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
 const db = require("../Config/Db");
@@ -10,10 +61,13 @@ function generateOTP() {
 
 // transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,     
   auth: {
-    user: process.env.EMAIL_USER, // email
-    pass: process.env.EMAIL_PASS, // app password
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
